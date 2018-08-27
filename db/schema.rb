2071440b312d9c7557d26846b9f4e1fc215d6faa
bao dang <baodq@atmarkcafe.org>
2018-08-27 10:52:15 +0700

    change associaton product and cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_081001) do
+ActiveRecord::Schema.define(version: 2018_08_27_025708) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
