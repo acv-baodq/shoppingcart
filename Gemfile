b643987ec2d707a2af25248a6b490360bf986bb5
bao dang <baodq@atmarkcafe.org>
2018-08-27 17:29:41 +0700

    config devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'jquery-rails'
 gem 'bootstrap', '~> 4.1.3'
 gem 'kaminari'
 gem "font-awesome-rails"
+gem 'devise'
 gem 'slim'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_061317) do
+ActiveRecord::Schema.define(version: 2018_08_27_081001) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
@@ -41,6 +41,22 @@ ActiveRecord::Schema.define(version: 2018_08_27_061317) do
     t.integer "category_id"
     t.string "img_url"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
